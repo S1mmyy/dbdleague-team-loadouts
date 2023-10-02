9fe9d0a6a3ca0439622e91e059b3dd66ca0de90d
S1mmyy <simmypoo385@gmail.com>
2023-10-02 15:40:33 -0400

    Initial implementation of perk drag and drop

--- js/page.js
+++ js/page.js
@@ -1,3 +1,4 @@
+// load killer data from file
 let killerData;
 async function getKillerData()
 {
@@ -30,4 +31,34 @@ killerSelectElement.onchange = function updateKillerImage()
   let killerImgLink = killerObj[0].characterImage;
 
   document.getElementById("killerImg").src = killerImgLink;
+}
+
+// define drag events
+let listBoxElements = document.getElementsByClassName("perkListBox");
+for (let i = 0; i < listBoxElements.length; i++)
+{
+  listBoxElements[i].addEventListener("dragover", dragover_handler);
+  listBoxElements[i].addEventListener("drop", drop_handler);
+}
+let perkImgElements = document.getElementsByClassName("perkImg");
+for (let i = 0; i < perkImgElements.length; i++)
+{
+  perkImgElements[i].addEventListener("dragstart", drag_handler);
+}
+
+function dragover_handler(ev)
+{
+  ev.preventDefault();
+}
+
+function drop_handler(ev)
+{
+  ev.preventDefault();
+  let data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function drag_handler(ev)
+{
+  ev.dataTransfer.setData("text", ev.target.id);
 }
